5c389b1bd52951bb8712b150b6d53d767d7cd139
David Thomas <d.thomas1@imperial.ac.uk>
2015-01-11 20:29:52 +0000

    Revert "Initial push." (from a completely different course...)
    This reverts commit 332de91ff8d90b8924efed40535a6c80f3bff537.

--- fragments/f_addu.c
+++
@@ -1,4 +0,0 @@
-#include <stdint.h>
-
-uint32_t f_addu(uint32_t a, uint32_t b)
-{ return a+b; }
--- fragments/f_fibonacci.c
+++
@@ -1,8 +0,0 @@
-#include <stdint.h>
-
-uint32_t f_fibonacci(uint32_t n)
-{
-	if(n<=1)
-		return n;
-	return f_fibonacci(n-1)+f_fibonacci(n-2);
-}
